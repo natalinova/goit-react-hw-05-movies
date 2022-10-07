fdd45607808efc7205ee049cdde4cb1115e9847b
Nataliya Novak <novak.natali@gmail.com>
2022-10-07 12:11:36 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Movies } from "../pages/Movies";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} >
+          <Route path="/" element={ <Home/>} />
+        </Route>
+        <Route path="/movies" element={ <Movies/>} /> 
+      </Routes>
+    </>
   );
 };
